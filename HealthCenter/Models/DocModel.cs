519e8d404a8fcd92293f0052d3cfcca30532f159
Sandeep Sagar Panjala <shanmukhreddychallapalli@gmail.com>
2023-11-29 21:01:29 -0600

    doctor model

---
+++ HealthCenter/Models/DocModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthCenter.Models
+{
+	public class DocModel
+	{
+        public int? DoctorId { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public string Phone { get; set; }
+    }
+}
+
