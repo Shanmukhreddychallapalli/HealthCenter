984dfb047e7ea8208c253acdd88c65d0d8c8bb1c
Sandeep Sagar Panjala <shanmukhreddychallapalli@gmail.com>
2023-11-29 21:01:01 -0600

    appointment model

---
+++ HealthCenter/Models/AppModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthCenter.Models
+{
+	public class AppModel
+	{
+        public int? AppointmentId { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public TimeSpan AppointmentTime { get; set; }
+        public string Notes { get; set; }
+    }
+}
+
