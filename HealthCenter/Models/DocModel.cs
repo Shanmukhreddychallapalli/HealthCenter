d139a28ed308c8eac49480e47df54f3e51ebcc32
Sandeep Sagar Panjala <shanmukhreddychallapalli@gmail.com>
2023-11-29 21:01:51 -0600

    patient model

--- HealthCenter/Models/DocModel.cs
+++ HealthCenter/Models/DocModel.cs
@@ -2,10 +2,10 @@
 {
 	public class DocModel
 	{
-        public int? DoctorId { get; set; }
-        public string Name { get; set; }
         public string Specialty { get; set; }
         public string Phone { get; set; }
+        public int? DoctorId { get; set; }
+        public string Name { get; set; }
     }
 }
 
---
+++ HealthCenter/Models/PatientModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HealthCenter.Models
+{
+	public class PatModel
+	{
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int? PatientId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        
+    }
+}
+
